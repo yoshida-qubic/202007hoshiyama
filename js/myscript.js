ba2efad7478dbaa958ba806578b3cc5176c88e32
Yoshida <qubic.co.ltd@gmail.com>
2020-07-14 10:55:44 +0900

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,27 @@
+
+'use strict';
+
+const w = $(window).width();
+const spwidth = 767;
+const tabletwidth = 1025;
+
+{
+	const openMenu = document.getElementById('menu_open');
+	const Nav = document.querySelector('header nav');
+
+	openMenu.addEventListener('click', function () {
+		openMenu.classList.toggle('active');
+		Nav.classList.toggle('active');
+	});
+}
+
+
+$(function () {
+
+// JQueryの範囲
+
+
+
+
+
+});
